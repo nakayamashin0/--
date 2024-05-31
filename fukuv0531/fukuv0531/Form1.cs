78c7b67a6240f91e1409f8710105b76b0c7a33ed
nakayamashin0 <167387930+nakayamashin0@users.noreply.github.com>
2024-05-31 13:35:52 +0900

    課題

--- fukuv0531/fukuv0531/Form1.cs
+++ fukuv0531/fukuv0531/Form1.cs
@@ -6,5 +6,23 @@ namespace fukuv0531
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int vy = -15;
+            MessageBox.Show($"{vy}");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int vy = 15;
+            MessageBox.Show($"{vy}");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top + 10;
+        }
     }
 }
