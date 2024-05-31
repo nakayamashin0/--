2150dbe98374cc1da650bda61a078cda75c34cc3
nakayamashin0 <167387930+nakayamashin0@users.noreply.github.com>
2024-05-31 13:16:30 +0900

    Initial commit

---
+++ cs0426/cs0426/Form1.cs
@@ -0,0 +1,21 @@
+namespace cs0426
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //‚¨‚Í‚æ‚¤‚Æ•\Ž¦
+            MessageBox.Show("‚±‚ñ‚É‚¿‚Í");
+        }
+    }
+}
---
+++ fukuv0524/fukuv0524/Form1.cs
@@ -0,0 +1,30 @@
+namespace fukuv0524
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top - 20;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left - 20;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left + 20;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top + 20;
+        }
+    }
+}
